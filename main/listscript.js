9f10e7aaa8c690d8e2ce938898a07c700b7b999c
Joanne-Chang <joachang@ucdavis.edu>
2023-05-20 14:29:41 -0700

    Fix null error

--- main/listscript.js
+++ main/listscript.js
@@ -13,6 +13,23 @@ if (element2) {
   }); 
 }
 
+const element3 = document.querySelector('.addTaskBtn');
+const element4 = document.querySelector('.removeTaskBtn');
+
+if (element3) {
+  document.querySelector('.addSiteBtn').addEventListener('click', function() {
+    addSiteItem();
+  });
+}
+
+if (element4) {
+  document.querySelector('.removeSiteBtn').addEventListener('click', function() {
+    removeSiteItem();
+  }); 
+}
+
+
+// Productive sites
 function addTaskItem () {  
   // Create new list item and append to the top
   var item = document.createElement("li");
@@ -27,7 +44,7 @@ function addTaskItem () {
     document.getElementById("taskList").insertAdjacentElement("afterbegin", item);
   
     // Alternate version for appending at the bottom (instead of top):
-    // document.getElementById("toDoList").appendChild(item); 
+    // document.getElementById("taskList").appendChild(item); 
   }
 }
 
@@ -36,10 +53,47 @@ function removeTaskItem() {
   var list = document.getElementById("taskList");
   var allItems = document.querySelectorAll("#taskList li");
   
-  // Remove item from top of list
-  list.removeChild(allItems[0]);
+  // Remove item from top of list (if list not empty)
+  if (allItems.length > 0) {
+    list.removeChild(allItems[0]);
+
+    // Alternate version to remove item from bottom of list
+    // var last = allItems.length - 1;
+    // list.removeChild(allItems[last]);
+  }
+}
+
+
+// Unproductive sites
+function addSiteItem () {  
+  // Create new list item and append to the top
+  var item = document.createElement("li");
+  var x = document.getElementById("addSiteInput").value;
   
-  // Alternate version to remove item from bottom of list
-  // var last = allItems.length - 1;
-  // list.removeChild(allItems[last]);
+  if (!x) {
+    return;
+  }
+  else {
+    item.innerHTML = x;
+ 
+    document.getElementById("sitesList").insertAdjacentElement("afterbegin", item);
+  
+    // Alternate version for appending at the bottom (instead of top):
+    // document.getElementById("sitesList").appendChild(item); 
+  }
+}
+
+function removeSiteItem() {
+  // Get the list and all of its items
+  var list = document.getElementById("sitesList");
+  var allItems = document.querySelectorAll("#sitesList li");
+  
+  // Remove item from top of list (if list not empty)
+  if (allItems.length > 0) {
+    list.removeChild(allItems[0]);
+
+    // Alternate version to remove item from bottom of list
+    // var last = allItems.length - 1;
+    // list.removeChild(allItems[last]);
+  }
 }
