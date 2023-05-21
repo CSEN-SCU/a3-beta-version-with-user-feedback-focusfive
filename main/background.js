412389110f7f32ca37efd5929d959ac09e4d75a4
Emily Wnag <xiaofang2146@gmail.com>
2023-05-21 11:56:06 -0700

    Apply different mode for youtube.com

--- main/background.js
+++ main/background.js
@@ -1,6 +1,16 @@
 // Retrieve the selected mode from storage or toggle state
 var mode = 'mindful'; // Replace with the actual method to retrieve the selected mode
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.mode) {
+        // Access the mode value received from togglescript.js
+        console.log('Mode received in background:', request.mode);
+        // Perform further actions with the mode value as needed
+        mode = request.mode;
+    }
+});
+
+
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
     if (details.url.includes('www.youtube.com') && mode === 'mindful') {
         // Open a webpage showing a reminder
