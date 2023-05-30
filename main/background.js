c5f65c61bf57f508a0d83e16b68f60d3c32d7120
Emily Wnag <xiaofang2146@gmail.com>
2023-05-29 19:42:07 -0700

    Rerouting get list from storage

--- main/background.js
+++ main/background.js
@@ -1,5 +1,6 @@
 // Retrieve the selected mode from storage or toggle state
 var mode = 'mindful'; // Replace with the actual method to retrieve the selected mode
+console.log("This is a log from background")
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.mode) {
@@ -12,14 +13,43 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-    if (details.url.includes('www.youtube.com') && mode === 'mindful') {
-        // Open a webpage showing a reminder
-        chrome.tabs.create({ url: '/popup/popup.html' });
-        chrome.tabs.remove(details.tabId); // Close the original tab
-    }
-    if (details.url.includes('www.redfin.com') && mode ===  'focus'){
-        console.log("this is a test log in background.js");
-        const redirectUrl = 'https://www.twitter.com';
-        chrome.tabs.update(details.tabId, { url: redirectUrl });
+    if (mode === 'mindful') {
+        if (isUnproductiveSite(details.url)) {
+            // Open a webpage showing a reminder
+            chrome.tabs.create({ url: '/popup/popup.html' });
+            chrome.tabs.remove(details.tabId); // Close the original tab
+        }
+    } else if (mode === 'focus') {
+        console.log("Background.js: this is focus mode. Start to redirect.")
+        isUnproductiveSite(details.url, function (isUnproductive) {
+            if (isUnproductive){
+                console.log("This is a test log in background.js");
+                const redirectUrl = 'https://www.scu.edu';
+                chrome.tabs.update(details.tabId, { url: redirectUrl });
+            }
+
+        });
     }
 });
+
+
+// Function to check if the URL belongs to an unproductive site
+function isUnproductiveSite(url, callback) {
+    getUnproductiveSites(function(sitesList) {
+        const isUnproductive = sitesList.some(site => url.includes(site));
+        callback(isUnproductive);
+    });
+}
+
+
+// Function to retrieve the unproductive sites list from Chrome storage
+function getUnproductiveSites(callback) {
+    chrome.storage.sync.get('unproductiveSites', function(result) {
+        const sitesList = result.unproductiveSites || [];
+        callback(sitesList);
+    });
+}
+
+
+
+
